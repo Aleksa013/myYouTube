8061c16625c86321631f96577b02e55d7a0da020
Aleksa013 <velma86@bk.ru>
2024-09-18 21:10:19 +0300

    feat: add footer

--- MY-YOUTUBE/src/app/app.component.ts
+++ MY-YOUTUBE/src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header/header.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HeaderComponent, MatSlideToggleModule],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HeaderComponent,
+    MatSlideToggleModule,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
