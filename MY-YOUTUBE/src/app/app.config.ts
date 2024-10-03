0ac2b35e3facf5c3bdda960e70430a258ec0224c
Aleksa013 <velma86@bk.ru>
2024-10-03 22:23:25 +0300

    feat/ change color of the theme

--- MY-YOUTUBE/src/app/app.config.ts
+++ MY-YOUTUBE/src/app/app.config.ts
@@ -33,6 +33,6 @@ export const appConfig: ApplicationConfig = {
       search: searchReducer,
     }),
     provideStoreDevtools(),
-    provideEffects(),
+    provideEffects(), provideAnimationsAsync(),
   ],
 };
--- MY-YOUTUBE/src/app/pages/login/login.component.ts
+++ MY-YOUTUBE/src/app/pages/login/login.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  public authForm: FormGroup = new FormGroup({});
+  constructor(private FB: FormBuilder) {
+    this.authForm = this.FB.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
+  public onSubmit() {
+    console.log(this.authForm.value);
+  }
 }
